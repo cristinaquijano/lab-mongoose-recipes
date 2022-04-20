aeb2f863843b909c8014665a93211459e8c0e6a5
Cristina Quijano <cristina@cristinas-air-2.kpn>
2022-04-20 18:44:33 +0200

    Iteration 4-5

--- index.js
+++ index.js
@@ -20,14 +20,24 @@ let newRecipe = {
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
+  .connect(MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTodopogy: true,
+  })
+  .then((self) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then((result) => {
+   return Recipe.insertMany(data)
+  })
+  .then((result) => {
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new:true})
+  })
+  .then((result) => {
+    return Recipe.findByIdAndDelete({title:"Carrot Cake"})
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
